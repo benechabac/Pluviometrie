7c2ee47659339c4589b2eac5f0f94049e16df610
PierreBdg <46017740+PierreBdg@users.noreply.github.com>
2019-01-11 15:09:57 +0100

    Update Pluviometrie.py

--- Pluviometrie.py
+++ Pluviometrie.py
@@ -160,7 +160,7 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     plt.close()
     
     body = json.dumps({
-            'title': 'Pluviométrie '+self.path_info[1], \
+            'title': '', \
             'img': '/'+fichier \
              });
 
