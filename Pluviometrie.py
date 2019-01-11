e01d5f841a194c795b8e943fb2f630c95604c78c
oussamaca <oussama.cherifi-alaoui@ecl18.ec-lyon.fr>
2019-01-11 16:33:55 +0100

    Ajout de la fonctionnalite de comparaison

--- Pluviometrie.py
+++ Pluviometrie.py
@@ -33,6 +33,10 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     elif self.path_info[0] == 'pluvio':
       self.send_pluvio()
       
+    # le chemin d'accès commence par /compare
+    elif self.path_info[0] == 'compare':
+      self.send_compare()
+
     # ou pas...
     else:
       self.send_static()
@@ -122,7 +126,6 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         return None
     else:
         sid = reg[0][0] # identifiant de la station
-
     
     # configuration du tracé
     fig1 = plt.figure(figsize=(18,9))
@@ -130,6 +133,7 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     ax.set_ylim(bottom=0,top=250)
     ax.grid(which='major', color='#888888', linestyle='-')
     ax.grid(which='minor',axis='x', color='#888888', linestyle=':')
+    # choix de la graduation en fonction du nombre d'annee a acfficher
     if fin==debut:
         ax.xaxis.set_major_locator(pltd.MonthLocator())
     elif fin-debut==1:
@@ -179,7 +183,106 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     headers = [('Content-Type','application/json')];
     self.send(body,headers)
 
+
+  def send_compare(self):
+    if len(self.path_info)>4:
+        debut = int(self.path_info[3])
+        fin = int(self.path_info[4])
+    else:
+        debut = 2011
+        fin = 2018
+
+    conn = sqlite3.connect('pluvio.sqlite')
+    c = conn.cursor()
+    
+    # On teste que la station demandée existe bien
+    c.execute("SELECT identifian FROM 'stations-pluvio-2018' WHERE nom=?", (self.path_info[1],))
+    reg = c.fetchall()
+    if len(reg)==0:
+        print ('Erreur nom')
+        self.send_error(404)    # Station non trouvée -> erreur 404
+        return None
+    else:
+        sid1 = reg[0][0] # identifiant de la station
+
+    # On teste que la station demandée existe bien pour la 2e
+    c.execute("SELECT identifian FROM 'stations-pluvio-2018' WHERE nom=?", (self.path_info[2],))
+    reg = c.fetchall()
+    if len(reg)==0:
+        print ('Erreur nom')
+        self.send_error(404)    # Station non trouvée -> erreur 404
+        return None
+    else:
+        sid2 = reg[0][0] # identifiant de la station
+
+    
+    # configuration du tracé
+    fig1 = plt.figure(figsize=(18,9))
+    ax = fig1.add_subplot(111)
+    ax.set_ylim(bottom=0,top=250)
+    ax.grid(which='major', color='#888888', linestyle='-')
+    ax.grid(which='minor',axis='x', color='#888888', linestyle=':')
+    # choix de la graduation en fonction du nombre d'annee a acfficher
+    if fin==debut:
+        ax.xaxis.set_major_locator(pltd.MonthLocator())
+    elif fin-debut==1:
+        ax.xaxis.set_major_locator(pltd.MonthLocator(range(1,13,2)))
+    elif fin-debut==2:
+        ax.xaxis.set_major_locator(pltd.MonthLocator(range(1,13,3)))
+    elif fin-debut==3:
+        ax.xaxis.set_major_locator(pltd.MonthLocator(range(1,13,4)))
+    elif fin-debut<=5:
+        ax.xaxis.set_major_locator(pltd.MonthLocator(range(1,13,6)))
+    else:
+        ax.xaxis.set_major_locator(pltd.YearLocator())
+    ax.xaxis.set_minor_locator(pltd.MonthLocator())
+    ax.xaxis.set_major_formatter(pltd.DateFormatter('%B %Y'))
+    ax.xaxis.set_tick_params(labelsize=10)
+    ax.xaxis.set_label_text("Date")
+    ax.yaxis.set_label_text("Hauteur de pluie mesurée (en mm)")
             
+    sta1 = "sta-"+sid1
+    # recupération de la date (mois-annee) et de la hauteur pour cette date
+    c.execute("SELECT SUBSTR(date,4,7) AS mois,SUM(`"+sta1+"`) FROM 'pluvio-histo-2018' WHERE`"+sta1+"_e`!='*' GROUP BY mois")
+    r = c.fetchall()
+    # recupération de la date (colonne 1) et transformation dans le format de pyplot
+    x = [pltd.date2num(dt.date(int(a[0][3:7]),int(a[0][:2]),1)) for a in r if int(a[0][3:7])>=debut and int(a[0][3:7])<=fin]
+    # récupération de la hauteur (colonne 2)
+    y1 = [ ( 0.0 if a[1]=='' else float(a[1]) ) for a in r if int(a[0][3:7])>=debut and int(a[0][3:7])<=fin]
+
+    sta2 = "sta-"+sid2
+    # recupération de la date (mois-annee) et de la hauteur pour cette date
+    c.execute("SELECT SUBSTR(date,4,7) AS mois,SUM(`"+sta2+"`) FROM 'pluvio-histo-2018' WHERE `"+sta2+"_e`!='*' GROUP BY mois")
+    r = c.fetchall()
+    # récupération de la hauteur (colonne 2)
+    y2 = [ ( 0.0 if a[1]=='' else float(a[1]) ) for a in r if int(a[0][3:7])>=debut and int(a[0][3:7])<=fin]
+
+    # trie selon date
+    x,y1 = zip(*sorted(zip(x, y1)))
+    x,y2 = zip(*sorted(zip(x, y2)))
+
+    # tracé de la courbe
+    plt.plot(x,y1,linewidth=1, linestyle='-', marker='o', color='blue', label=self.path_info[1])
+    plt.plot(x,y2,linewidth=1, linestyle='-', marker='o', color='red', label=self.path_info[2])
+        
+    # légendes
+    plt.legend(loc='lower left')
+    plt.title('Pluviométrie '+self.path_info[1]+' et '+self.path_info[2],fontsize=16)
+
+    # génération des courbes dans un fichier PNG
+    fichier = 'courbes/compare_'+self.path_info[1]+'_'+self.path_info[2]+'_'+str(debut)+'_'+str(fin)+'.png'
+    plt.savefig('client/{}'.format(fichier))
+    plt.close()
+    
+    body = json.dumps({
+            'title': '', \
+            'img': '/'+fichier \
+             });
+
+    # on envoie
+    headers = [('Content-Type','application/json')];
+    self.send(body,headers)
+         
     
   #
   # On envoie les entêtes et le corps fourni
